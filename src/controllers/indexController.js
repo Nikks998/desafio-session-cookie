750d6f936cbcee6d4d7462192bd6100bd86299db
Caceres Matias <kcerescmh98@gmail.com>
2023-09-05 19:55:22 -0300

    feat: express-validator instalacion

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,26 @@
+const { validationResult } = require('express-validator')
+
 module.exports = {
     index: (req, res) => {
-        res.render('index');
+        res.render('index')
+    },
+
+    indexCreate: (req, res) => {
+        const errors = validationResult(req)
+        if (errors.isEmpty()) {
+            const name = req.body.name
+            const email = req.body.email
+            const age = req.body.age
+            const color = req.body.color
+            
+            req.session.usuario = {
+                name,
+                email,
+                age,
+                color
+            }
+        } else {
+            res.render('index')
+        }
     }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 const { index } = require('../controllers/indexController');
+const userValidator = require('../validations/userValidator');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', index);
+router.post('/', userValidator, index)
 
 module.exports = router;
